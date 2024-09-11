dc070da740a1ab532cf3e72aadc54a0bbaed438f
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2024-09-10 20:33:48 -0400

    Hide slider buttons if there no more slides to scroll

--- src/js/img-slider.js
+++ src/js/img-slider.js
@@ -1,10 +1,23 @@
 const slidesContainer = document.querySelector('.img-slider .slides-container');
 const slideButtons = document.querySelectorAll('.img-slider .slide-btn');
 
-slideButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const direction = button.classList.contains('prev') ? -1 : 1;
-    const scrollAmount = slidesContainer.clientWidth * direction;
-    slidesContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+window.addEventListener('load', initSlider);
+
+function initSlider() {
+  slideButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const direction = button.classList.contains('prev') ? -1 : 1;
+      const scrollAmount = slidesContainer.clientWidth * direction;
+      slidesContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    });
   });
-});
+
+  const handleSliderButtons = function () {
+    const maxScrollLeft = slidesContainer.scrollWidth - slidesContainer.clientWidth;
+    slideButtons[0].style.display = slidesContainer.scrollLeft <= 0 ? 'none' : 'flex';
+    slideButtons[1].style.display = slidesContainer.scrollLeft >= maxScrollLeft ? 'none' : 'flex';
+  };
+
+  slidesContainer.addEventListener('scroll', handleSliderButtons);
+  handleSliderButtons();
+}
